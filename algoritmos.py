42451f8b94d881b7a1766c48824cf929b79e3cad
llullanqui <luis.1142@hotmail.com>
2018-06-06 20:44:13 -0500

    atbash

--- algoritmos.py
+++ algoritmos.py
@@ -11,6 +11,14 @@ def caesar(ciphertext):
             decifrado += chr(val)
     return decifrado
 
+def atbash(ciphertext):
+    decifrado = ""
+    for caracter in ciphertext:
+        if ord(caracter)<ord('A') or ord(caracter)>ord('Z'):
+            decifrado += caracter
+        else:
+            decifrado += chr(ord('A') + ord('Z') - ord(caracter))
+    return decifrado
 
-
-print("deciframiento: ", caesar(input("Texto a decifrar con Caesar:")))
+#print("deciframiento: ", caesar(input("Texto a decifrar con Caesar: ")))
+#print("deciframiento: ", atbash(input("Texto a decifrar con atbash: ")))
