b2f3eef4e53f701e5761f4a0ea02fdb1196a0c68
llullanqui <luis.1142@hotmail.com>
2018-06-10 09:56:05 -0500

    number code

--- algoritmos.py
+++ algoritmos.py
@@ -64,7 +64,34 @@ listaDeSoluciones = ["WELCOMETOGRAVITYFALLS.",
                      ]
 
 def numberCode(ciphertext):
+    ciphertext += " "
     decifrado = ""
+    lista = [] #lista[0] indice del cap, el resto son los indices de las letras
+    actual = ""
+    for caracter in ciphertext:
+        if not ord('0') <= ord(caracter) <= ord('9'):
+            if caracter == ")":
+                lista.append([actual])
+                actual=""
+            elif caracter == ",":
+                lista[-1].append(actual)
+                actual=""
+            elif caracter == "]":
+                lista[-1].append(actual)
+                lista[-1].append(" ")
+                actual=""
+            elif caracter == " " and actual != "":
+                lista[-1].append(actual)
+                actual=""
+        else:
+            actual+=caracter
+
+    for subLista in lista:
+        for item in subLista[1:]:
+            if item == " ":
+                decifrado += " "
+            else:
+                decifrado += listaDeSoluciones[int(subLista[0])-1][int(item)-1]
     return decifrado
 
 def vigenere(ciphertext):
@@ -75,3 +102,4 @@ def vigenere(ciphertext):
 #print("deciframiento: ", atbash(input("Texto a decifrar con atbash: ")))
 #print("deciframiento: ", A1Z26(input("Texto a decifrar con A1Z26: ")))
 #print("combined: ", combined(input("Texto a decifrar con combined cipher: ")))
+print("number code:",numberCode(input("Texto a decifrar con number code: ")))
